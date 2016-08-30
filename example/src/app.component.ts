524768ec1c187172daf6856bb26ae02a71cb2d57
Janne Julkunen <janne.julkunen@zef.fi>
2016-08-30 17:05:16 +0300

    Fixed template loading

--- example/src/app.component.ts
+++ example/src/app.component.ts
@@ -2,8 +2,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: require('app.component.html'),
-  styles: [require('app.component.css')]
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
   private title = 'Simple example app for the angular2-dropzone-wrapper';
