1d27d45ed170b31c5b953063048fb6439acb191e
ZEF Devel <devel@zef.fi>
2016-10-05 22:04:21 +0300

    Publishing 0.3.0

--- main.js
+++ main.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:8080/";
+/******/ 	__webpack_require__.p = "https://zefoy.github.io/angular2-dropzone-wrapper/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
