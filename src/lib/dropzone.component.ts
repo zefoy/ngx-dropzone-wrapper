345cc3e51d521620b0b7805a62dc770f088d557e
Janne Julkunen <janne.julkunen@zef.fi>
2016-12-27 13:37:55 +0200

    Removed unneeded host style from the component

--- src/lib/dropzone.component.ts
+++ src/lib/dropzone.component.ts
@@ -10,10 +10,7 @@ import { DropzoneEvents, DropzoneConfig, DropzoneConfigInterface } from './dropz
   selector: 'dropzone',
   templateUrl: './dropzone.component.html',
   styleUrls: ['./dropzone.component.css', '../../node_modules/dropzone/dist/min/dropzone.min.css'],
-  encapsulation: ViewEncapsulation.None,
-  host: {
-    style: 'display: block;'
-  }
+  encapsulation: ViewEncapsulation.None
 })
 export class DropzoneComponent implements OnInit, DoCheck, OnDestroy, OnChanges {
   public dropzone: any;
