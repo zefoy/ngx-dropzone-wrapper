0797778cf1ea3dec1eaa8ae361f083ffc932d39c
Janne Julkunen <janne.julkunen@zef.fi>
2017-12-11 14:29:18 +0200

    Added exportAs definitions for directive and component

--- src/lib/dropzone.component.ts
+++ src/lib/dropzone.component.ts
@@ -5,6 +5,7 @@ import { DropzoneConfigInterface } from './dropzone.interfaces';
 
 @Component({
   selector: 'dropzone',
+  exportAs: 'ngxDropzone',
   templateUrl: './dropzone.component.html',
   styleUrls: ['./dropzone.component.css'],
   encapsulation: ViewEncapsulation.None
